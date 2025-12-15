70838afd72061bfb11dc72bda32e613339f12d4d
Pedro de Bastos <pedro.mar.bas@gmail.com>
2025-12-15 12:26:10 -0800

    Updated graph physics

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -43,29 +43,29 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph({
       localGraph: {
-        drag: true, // whether to allow panning the view
-        zoom: true, // whether to allow zooming in and out
-        depth: 1, // how many hops of notes to display
-        scale: 1.1, // default view scale
-        repelForce: 0.5, // how much nodes should repel each other
-        centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
-        fontSize: 0.6, // what size should the node labels be?
-        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+        drag: true,
+        zoom: true,
+        depth: 1,
+        scale: 1.1,
+        repelForce: 0.8, // Increased from 0.5 to spread nodes out
+        centerForce: 0.1, // Drastically reduced from 0.3 to prevent clumping
+        linkDistance: 40, // Increased to allow more space
+        fontSize: 0.6,
+        opacityScale: 1,
         removeTags: ["no-graph"],
-        showTags: true, // whether to show tags in the graph
+        showTags: true,
       },
       globalGraph: {
         drag: true,
         zoom: true,
         depth: -1,
         scale: 0.9,
-        repelForce: 0.5,
-        centerForce: 0.3,
-        linkDistance: 30,
+        repelForce: 0.8,
+        centerForce: 0.1,
+        linkDistance: 40,
         fontSize: 0.6,
         opacityScale: 1,
-        removeTags: ["no-graph"], // excluding notes with this tag
+        removeTags: ["no-graph"],
         showTags: true,
       },
     }),
